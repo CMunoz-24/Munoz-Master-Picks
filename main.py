2e359cc5c1d6e31ec895ed004d8ad60c09b8ef5c
CMunoz-24 <cmunoz0024@gmail.com>
2025-05-02 21:10:40 -0700

    Fix redirect and syntax issues

--- main.py
+++ main.py
@@ -111,7 +111,6 @@ from flask import redirect
 def index():
     return redirect("/home")
 
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
